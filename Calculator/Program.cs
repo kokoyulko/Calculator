c3f80d878b65cc66325d1f7b7f3538a702b182c1
yu.shevchenko <yu.shevchenko@temabit.com>
2019-08-14 16:10:58 +0300

    1

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,7 +10,7 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            ////////kuku\\\\\\
+            ////////1\\\\\\
             double a;
             double b;
             double res = 0;
